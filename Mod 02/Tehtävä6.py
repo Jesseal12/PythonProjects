7dbd99df9b20116c2a6b29b6d7176cca9de4d559
Jesseal12 <148433549+Jesseal12@users.noreply.github.com>
2025-08-20 15:01:53 +0300

    update

--- Mod 02/Tehtävä6.py
+++ Mod 02/Tehtävä6.py
@@ -7,6 +7,6 @@ neljaArvo1 = random.randint(1,6)
 neljaArvo2 = random.randint(1,6)
 neljaArvo3 = random.randint(1,6)
 neljaArvo4 = random.randint(1,6)
-#parempi tapa olis varmaan saada for looping kautta palautettu arvo printeille
+#parempi tapa olis varmaan saada for looping kautta listaan random intin
 print(f"{kolmeArvo1}{kolmeArvo2}{kolmeArvo3}")
 print(f"{neljaArvo1}{neljaArvo2}{neljaArvo3}{neljaArvo4}")
