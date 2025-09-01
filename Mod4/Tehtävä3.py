f1eafbdcc7d78ecc91de56f720651c975063c275
Jesseal12 <148433549+Jesseal12@users.noreply.github.com>
2025-09-01 18:40:12 +0300

    update

--- Mod4/Tehtävä3.py
+++ Mod4/Tehtävä3.py
@@ -11,16 +11,18 @@ while syottoArvo != 0 :
         maxArvo=syottoArvo
         arvot.insert(0,minArvo)
         arvot.insert(1,maxArvo)
-        syottoArvo = float(input("Anna niin monta lukuarvoa kuin haluat : "))
+        syottoArvo = float(input("Anna lukuarvo ja lopeta 0:lla : "))
     elif syottoArvo<minArvo :
         minArvo =syottoArvo
         arvot.insert(0, minArvo)
-        syottoArvo = float(input("Anna niin monta lukuarvoa kuin haluat : "))
+        syottoArvo = float(input("Anna lukuarvo ja lopeta 0:lla : "))
 
     elif syottoArvo>maxArvo :
          maxArvo = syottoArvo
          arvot.insert(1, maxArvo)
-         syottoArvo = float(input("Anna niin monta lukuarvoa kuin haluat : "))
+
+
+         syottoArvo = float(input("Anna lukuarvo ja lopeta 0:lla : "))
    # bug se jatkuvasti printaa 0 molemille arvoille. Yritin korjata lisäämällä listaan
     #jos se johtuisi siitä et loopissa ei pysy muistissa
     print(f"Pienin arvo on : {arvot[0]:0.2f} ja suurin arvo on : {arvot[1]:0.2f}")
