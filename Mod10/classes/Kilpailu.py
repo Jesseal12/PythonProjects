df2cd462a292822f43a77187bba3c53dd211a510
Jesseal12 <148433549+Jesseal12@users.noreply.github.com>
2025-10-26 23:30:30 +0200

    update

--- Mod10/classes/Kilpailu.py
+++ Mod10/classes/Kilpailu.py
@@ -1,3 +1,43 @@
+import random
+
+from Mod9.Classes.Car import Car
+from Mod9.Tehtävä4 import kokonaismatka
+
+
 class Kilpailu :
-    def __init__(self,nimi,):
-        self.XD = print("owo")
+    def __init__(self,nimi,metrit,autolista):
+        self.nimi = nimi
+        self.kilometriMäärä = metrit/1000
+        self.autoLista = []
+
+        for i in range(autolista):
+            randomSpeed = random.randint(100,200)
+            Car("ABC"+str(i+1),randomSpeed)
+
+
+    def tunti_kuluu(self,tuntiMäärä):
+
+        for auto in self.autoLista:
+
+            kokonaismatka = 0
+            while kokonaismatka < self.kilometriMäärä:
+                nopeusmuutos = random.randint(-10, 15)
+                auto.kiihdyta(nopeusmuutos)
+                auto.kulje(tuntiMäärä)
+                kokonaismatka += auto.matka/1000
+
+                print(f"{auto.rekisteri} on kulkenut {kokonaismatka:0.2f}m")
+
+
+    def tulosta_tilanne(self):
+        for auto in self.autoLista :
+            print(f"auto :{auto.rekisteri} on kulkenut {kokonaismatka:0.2f}km  ")
+
+
+    def kilpailu_ohi(self):
+        for auto in self.autoLista :
+            if kokonaismatka>=self.kilometriMäärä :
+                return True
+            else:
+                return False
+        return
---
+++ Mod10/tehtävä4.py
@@ -0,0 +1,5 @@
+from classes.Kilpailu import Kilpailu
+
+uusiKilpailu =Kilpailu("WorldTour",10000,10)
+
+print(f"Kilpailu {uusiKilpailu.nimi} on alkanut!")
